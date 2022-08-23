a2aee6404ef199014b1fff44fad1eb20cbcb5bf2
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-23 11:36:48 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+
+def greet_programmer 
+     puts("Hello, programmer!");
+ end
+ def greet(name = "Jimmy")
+     puts "Hello, #{name}!"
+   end
+ def greet_with_default(name ="Naureen")
+     puts "Hello, #{name}!"
+   end
+ def greet_with_default (name ="programmer!")
+     puts "Hello, #{name}!"
+   end
+ def add(num1, num2)
+     return num1 + num2
+   end
+   sum1 = add(4,3)
+   def halve(x)
+     if x.is_a?(Integer);
+       x/2
+     else
+       nil
+     end
+   end
+   prod =halve(6);
--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+-require_relative 'spec_helper'
 require_relative '../methods'
 
 describe '#greet_programmer' do
